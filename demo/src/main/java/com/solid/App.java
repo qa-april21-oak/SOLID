aa835a18bb0101b073090bbf1476c89852ee0984
jonathanowolabi <jowolabi@qa.com>
2021-05-17 16:07:47 +0100

    JO - code inprovements for demo

--- demo/src/main/java/com/solid/App.java
+++ demo/src/main/java/com/solid/App.java
@@ -1,13 +1,14 @@
 package com.solid;
 
-/**
- * Hello world!
- *
- */
+
 public class App 
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+    public static void main( String[] args ) {
+    	
+    	Customer bob = new Customer("Bob", 94, "1 abc lane, def, se17 5hg");
+		Customer james = new Customer("James", 23, "6 abc lane, def, wd17 5hg");
+		
+		System.out.println(bob.toString());
+		System.out.println(james.toString());
     }
 }
